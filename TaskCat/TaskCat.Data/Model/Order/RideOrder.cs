2e3d13469e68d08a3b3398da9ccc79c9f7b79ed2
Swagata Prateek <s.prateek@hotmail.com>
2016-01-14 16:07:49 +0600

    Kept orders as models, added Basic RideOrderModel, need to resolve that

---
+++ TaskCat/TaskCat.Data/Model/Order/RideOrder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskCat.Data.Model.Order
+{
+    public class RideOrder : OrderModel
+    {
+        public RideOrder(string name = null) : base(name, "Ride")
+        {
+
+        }
+
+        public Location From { get; set; }
+        public Location To { get; set; }
+
+        /// <summary>
+        /// Basically a list of vehicles users want to avail and all of these options 
+        /// are basically
+        /// a or relation
+        /// </summary>
+        public List<string> VehiclePreference { get; set; }
+
+    }
+
+    //FIXME: This really shouldnt be done this way man, 
+    //we really need to tie something cool here for all vehicles
+    public class VehiclePreference
+    {
+        public const string CNG = "CNG";
+        public const string SEDAN = "SEDAN";
+    }
+}
---
+++ TaskCat/TaskCat.Data/Model/OrderModel.cs
@@ -0,0 +1,43 @@
+﻿namespace TaskCat.Data.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class OrderModel
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_name))
+                    return string.Concat("Ride Request for ", _user);
+                return _name;
+            }
+
+            set { _name = value; }
+        }
+
+        public string Type { get; set; }
+
+        /// <summary>
+        /// This type would be used for multiple models for single order purpose
+        /// All the requests now support json, at some point, we might have to support
+        /// Form submits
+        /// </summary>
+        private string _payloadType = "default";
+        public string PayloadType { get { return _payloadType; } set { _payloadType = value; } }
+
+        private string _user = "Anonymous";
+        public string User { get { return _user; } set { _user = value; } }
+
+        public OrderModel(string name, string type)
+        {
+            this.Type = type;
+            this.Name = name;
+        }
+    }
+}
