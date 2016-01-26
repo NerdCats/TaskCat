081b87a41ca291290d3d4557a8bb63248ad53c03
Swagata Prateek <s.prateek@hotmail.com>
2016-01-26 17:08:37 +0600

    Added Identity Provider and started added Auth repo too

---
+++ TaskCat/TaskCat/Lib/Auth/AuthRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace TaskCat.Lib.Auth
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using TaskCat.Data.Entity.Identity;
+
+    public class AuthRepository
+    {
+        internal Client FindClient(string clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<User> FindUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<bool> AddRefreshToken(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<RefreshToken> FindRefreshToken(string hashedTokenId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<bool> RemoveRefreshToken(string hashedTokenId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskCat/TaskCat/Lib/Identity/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,168 @@
+﻿namespace TaskCat.Lib.Identity
+{
+    using Auth;
+    using Microsoft.Owin.Security.OAuth;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Threading.Tasks;
+    using Data.Entity.Identity;
+    using Data.Model.Identity;
+    using Utility;
+    using System.Security.Claims;
+    using Microsoft.Owin.Security;
+
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly AuthRepository authRepository;
+
+        public SimpleAuthorizationServerProvider(AuthRepository authRepository)
+        {
+            this.authRepository = authRepository;
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            var clientId = default(string);
+            var clientSecret = default(string);
+            var client = default(Client);
+
+            if (!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.TryGetFormCredentials(out clientId, out clientSecret);
+            }
+
+            if (context.ClientId == null)
+            {
+                // INFO: Remove the comments from the below line context.Validated(), and validate context 
+                // if you dont want to force sending clientId/secrects once obtain access tokens. 
+                // context.Validated();
+
+                context.SetError("invalid_clientId", "ClientId not present");
+                return Task.FromResult<object>(null);
+            }
+
+            client = authRepository.FindClient(context.ClientId);
+
+            if (client == null)
+            {
+                context.SetError("invalid_clientId", string.Format("Client '{0}' is not registered in the system.", context.ClientId));
+
+                return Task.FromResult<object>(null);
+            }
+
+            if (client.ApplicationType == ApplicationTypes.Android || client.ApplicationType==ApplicationTypes.IOS)
+            {
+                if (string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    context.SetError("invalid_clientId", "Client secret should be sent.");
+
+                    return Task.FromResult<object>(null);
+                }
+                else
+                {
+                    if (client.Secret != HashMaker.GetHash(clientSecret))
+                    {
+                        context.SetError("invalid_clientId", "Client secret is invalid.");
+
+                        return Task.FromResult<object>(null);
+                    }
+                }
+            }
+
+            if (!client.Active)
+            {
+                context.SetError("invalid_clientId", "Client is inactive.");
+
+                return Task.FromResult<object>(null);
+            }
+
+            context.OwinContext.Set<string>("as:clientAllowedOrigin", client.AllowedOrigin);
+            context.OwinContext.Set<string>("as:clientRefreshTokenLifeTime", client.RefreshTokenLifeTime.ToString());
+
+            context.Validated();
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+
+            if (allowedOrigin == null) allowedOrigin = "*";
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            var user = await authRepository.FindUser(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            identity.AddClaim(new Claim("sub", context.UserName));
+
+            // FIXME: I really dont know here, just adding a dummy role here, 
+            // although this is a global login though, Im not sure whether Id use the other roles here
+
+            identity.AddClaim(new Claim("role", "user"));
+
+            var props = new AuthenticationProperties(
+                new Dictionary<string, string>
+                {
+                    {
+                        "as:client_id", (context.ClientId == null) ? string.Empty : context.ClientId
+                    },
+                    {
+                        "userName", context.UserName
+                    }
+                });
+
+            var ticket = new AuthenticationTicket(identity, props);
+
+            context.Validated(ticket);
+        }
+
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            var originalClient = context.Ticket.Properties.Dictionary["as:client_id"];
+            var currentClient = context.ClientId;
+
+            if (originalClient != currentClient)
+            {
+                context.SetError("invalid_clientId", "Refresh token is issued to a different clientId.");
+
+                return Task.FromResult<object>(null);
+            }
+
+            // INFO: Change auth ticket for refresh token requests
+            var newIdentity = new ClaimsIdentity(context.Ticket.Identity);
+            
+            // FIXME: Need to figure out how would I actually handle things here
+            // This is a refresh token here, need to add proper claims and values here, but Im stuck like a moron
+            newIdentity.AddClaim(new Claim("newClaim", "newValue"));
+
+            var newTicket = new AuthenticationTicket(newIdentity, context.Ticket.Properties);
+
+            context.Validated(newTicket);
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+    }
+}
