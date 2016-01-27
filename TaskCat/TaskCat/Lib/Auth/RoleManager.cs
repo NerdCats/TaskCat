177a542015b19f7dbc65be7d673ddf29b02e867e
Swagata Prateek <thehoneymad@hotmail.com>
2016-01-27 14:48:50 +0600

    MINOR: Changes in namespace inclusion

--- TaskCat/TaskCat/Lib/Auth/RoleManager.cs
+++ TaskCat/TaskCat/Lib/Auth/RoleManager.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TaskCat.Lib.Auth
+﻿namespace TaskCat.Lib.Auth
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
     public class RoleManager
     {
     }
--- TaskCat/TaskCat/Lib/Db/CollectionNames.cs
+++ TaskCat/TaskCat/Lib/Db/CollectionNames.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TaskCat.Lib.Db
+﻿namespace TaskCat.Lib.Db
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
     public class CollectionNames
     {
         public const string JobsCollectionName = "Jobs";
