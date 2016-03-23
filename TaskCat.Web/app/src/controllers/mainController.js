9c652fca24498ef410662e2092ae8b9c2e49a327
Swagata Prateek <s.prateek@hotmail.com>
2016-03-23 17:01:27 +0600

    Added a simple directive to load background-img and tested it with supported orders

--- TaskCat.Web/app/src/controllers/mainController.js
+++ TaskCat.Web/app/src/controllers/mainController.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular
-  .module('app')
-  .controller('mainController', mainController);
+    .module('app')
+    .controller('mainController', mainController);
 
   mainController.$inject = ['$mdSidenav', 'orderService', '$log'];
 
@@ -11,19 +11,18 @@
   function mainController($mdSidenav, orderService, $log) {
     var vm = this;
     vm.toggleSideNav = toggleSideNav;
-    vm.orders = orderService.getSupportedOrders();
 
     activate();
 
     function activate() {
       $log.info('Fetching Supported Orders');
       return orderService.getSupportedOrders().then(function(response) {
-        $log.info('Fetched '+ response.data.length +' supported orders');
-        vm.orders = data;
+        $log.info('Fetched ' + response.data.length + ' supported orders');
+        vm.orders = response.data;
         return vm.orders;
-      }, function(error){
-        $log.error('Unable to load customer data: '+ error.message);
-        vm.status='Unable to load customer data: '+ error.message;
+      }, function(error) {
+        $log.error('Unable to load customer data: ' + error.message);
+        vm.status = 'Unable to load customer data: ' + error.message;
       });
     }
 
