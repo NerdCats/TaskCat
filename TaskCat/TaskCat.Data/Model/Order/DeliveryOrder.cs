3904b424316121eee4f1f284ee09ed8aee6e351d
Swagata Prateek <s.prateek@hotmail.com>
2016-03-24 14:59:30 +0600

    Adding DeliveryOrder

---
+++ TaskCat/TaskCat.Data/Model/Order/DeliveryOrder.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskCat.Data.Model.Order
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DeliveryOrder : OrderModel
+    {
+        public Location From { get; set; }
+        public Location To { get; set; }
+
+        public string PackageDescription { get; set; }
+        public double PackageWeight { get; set; }
+
+        public DeliveryOrder(string name = null) : base(name, "Delivery")
+        {
+        }
+    }
+}
---
+++ TaskCat/TaskCat/Lib/Job/Builders/DeliveryJobBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskCat.Lib.Job.Builders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class DeliveryJobBuilder : JobBuilder
+    {
+        public override void BuildTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
