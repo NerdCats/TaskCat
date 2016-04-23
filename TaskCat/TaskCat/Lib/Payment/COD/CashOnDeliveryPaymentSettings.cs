40475dc51e4c64d1d1f0c5c6df5b6f118e875c48
Swagata Prateek <thehoneymad@hotmail.com>
2016-04-23 16:00:16 +0600

    #9 made a simple payment manager as this would suffice

--- TaskCat/TaskCat/Lib/Payment/COD/CashOnDeliveryPaymentSettings.cs
+++ TaskCat/TaskCat/Lib/Payment/COD/CashOnDeliveryPaymentSettings.cs
@@ -6,7 +6,7 @@
         /// <summary>
         /// Gets or sets a value indicating whether to "additional fee" is specified as percentage. true - percentage, false - fixed value.
         /// </summary>
-        public bool AdditionalFeePercentage { get; set; }
+        public bool IsAdditionalFeeSetOnPercentage { get; set; }
         /// <summary>
         /// Additional fee
         /// </summary>
--- TaskCat/TaskCat/Lib/Payment/PaymentManager.cs
+++ TaskCat/TaskCat/Lib/Payment/PaymentManager.cs
@@ -1,14 +1,29 @@
 ﻿namespace TaskCat.Lib.Payment
 {
-    using System;
     using System.Collections.Generic;
     using Data.Lib.Payment;
+    using Manual;
 
     public class PaymentManager : IPaymentManager
     {
+        private IList<IPaymentMethod> paymentMethods;
+
+        public PaymentManager()
+        {
+            paymentMethods = new List<IPaymentMethod>();
+            paymentMethods.Add(
+                new CashOnDeliveryPaymentMethod(
+                    new CashOnDeliveryPaymentSettings()
+                    {
+                        AdditionalFee = 2,
+                        IsAdditionalFeeSetOnPercentage = true,
+                        TransactMode = TransactMode.AuthorizeAndCapture
+                    }));
+        }
+
         public IList<IPaymentMethod> GetPaymentMethods()
         {
-            throw new NotImplementedException();
+            return paymentMethods;
         }
     }
 }
