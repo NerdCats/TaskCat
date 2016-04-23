a485236dcf0b981463e83a91f58d5c5414e127d2
Swagata Prateek <thehoneymad@hotmail.com>
2016-04-23 23:26:57 +0600

    #9 Documentation update for Orders

--- TaskCat/TaskCat.Data/Model/Order/DeliveryOrder.cs
+++ TaskCat/TaskCat.Data/Model/Order/DeliveryOrder.cs
@@ -19,9 +19,19 @@
             get { return PackageList == null ? 0 : PackageList.Sum(x => x.Weight); }
         }
 
+        /// <summary>
+        /// Package List to describe what Items would be delivered
+        /// </summary>
+        /// <remarks>
+        /// Price field would be ignored if the order is from a regular User and 
+        /// would only be considered if the request is from an Enterprise User
+        /// </remarks>
         [Required]
         public List<ItemDetails> PackageList { get; set; }
 
+        /// <summary>
+        /// Note to delivery man to provide extra info to delivery man
+        /// </summary>
         public string NoteToDeliveryMan { get; set; }
 
         public DeliveryOrder(string name = null) : base(name, "Delivery")
--- TaskCat/TaskCat.Data/Model/OrderModel.cs
+++ TaskCat/TaskCat.Data/Model/OrderModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace TaskCat.Data.Model
 {
-    using GeoJson;
     using MongoDB.Bson.Serialization.Attributes;
     using System;
     using System.ComponentModel.DataAnnotations;
+    using Geocoding;
 
     [BsonIgnoreExtraElements(Inherited = true)]
     public abstract class OrderModel
@@ -29,26 +29,32 @@
         [Required(ErrorMessage ="Order Type not provided" )]
         public string Type { get; set; }
 
+        
+        private string _payloadType = "default";
         /// <summary>
         /// This type would be used for multiple models for single order purpose
         /// All the requests now support json, at some point, we might have to support
         /// Form submits
         /// </summary>
-        private string _payloadType = "default";
         public string PayloadType { get { return _payloadType; } set { _payloadType = value; } }
 
+        
+        private string _userId;
         /// <summary>
         /// UserId that the order is created for
         /// </summary>
-        private string _userId;
+        [Required(ErrorMessage = "Order Type not provided")]
         public string UserId { get { return _userId; } set { _userId = value; } }
 
         /// <summary>
         /// Location where the order was originated from
         /// </summary>
-        public Point OrderLocation { get; set; }
+        public DefaultAddress OrderLocation { get; set; }
 
         private DateTime? _eta;
+        /// <summary>
+        /// ETA suggested for Order
+        /// </summary>
         public DateTime? ETA
         {
             get { return _eta; }
@@ -60,6 +66,9 @@
         }
 
         private double? _ETAMinutes;
+        /// <summary>
+        /// ETA to be described as minutes 
+        /// </summary>
         public double? ETAMinutes {
             get {
                 return _ETAMinutes;
@@ -71,6 +80,13 @@
             }
         }
 
+        /// <summary>
+        /// A supported payment method id
+        /// </summary>
+        /// <remarks>
+        /// A supported payment method list can be accessed GET /api/payment
+        /// </remarks>
+        /// 
         public string PaymentMethod { get; set; }
 
         public OrderModel()
