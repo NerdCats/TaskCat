656cad0c111951318ceb22665399fda87175d6d0
Fahim Masud Choudhury <fahim@gobd.co>
2016-04-24 13:54:54 +0600

    #17 Added a very basic IServiceChargeCalculationService implementation

---
+++ TaskCat/TaskCat/Lib/Order/EnterpriseDelivery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskCat.Lib.Order
+{
+    public class EnterpriseDelivery
+    {
+    }
+}
---
+++ TaskCat/TaskCat/Lib/Order/ManualDelivery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskCat.Data.Model.Inventory;
+
+namespace TaskCat.Lib.Order
+{
+    public class ManualDelivery : IServiceChargeCalculationService
+    {
+        List<ItemDetails> Items;
+
+        public ManualDelivery(List<ItemDetails> items)
+        {
+            this.Items = items;
+        }
+
+        public double CalculateServiceCharge()
+        {
+            double ServiceCharge;
+
+            if(Items != null)
+            {
+                foreach(ItemDetails item in Items)
+                {
+
+                }
+            }
+
+
+            return ;
+        }
+    }
+}
