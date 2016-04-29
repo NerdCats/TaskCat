7fcc7dec6247fde1428396d1c3ec383853773c70
Swagata Prateek <thehoneymad@hotmail.com>
2016-04-29 23:31:52 +0600

    Added a Settings interface so production and local settings are different

---
+++ TaskCat/TaskCat/App/Settings/ProprietorSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskCat.App.Settings
+{
+    using Data.Model.Geocoding;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProprietorSettings
+    {
+        public ProprietorSettings()
+        {
+                
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DefaultAddress Address { get; set; }
+        [Required]
+        public string CultureCode { get; set; }
+    }
+}
