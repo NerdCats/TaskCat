5426605f25901b1dcda57ea1a4516dc82202bec2
Swagata Prateek <s.prateek@hotmail.com>
2016-05-04 14:50:19 +0600

    MINOR: Cleanup AcccountController

--- TaskCat/TaskCat/Controller/Auth/AccountController.cs
+++ TaskCat/TaskCat/Controller/Auth/AccountController.cs
@@ -55,27 +55,19 @@
         [Route("Register")]
         public async Task<IHttpActionResult> Register(RegistrationModelBase userModel)
         {
-            try
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await accountRepository.RegisterUser(userModel);
+
+            var errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
             {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
-
-                var result = await accountRepository.RegisterUser(userModel);
-
-                var errorResult = GetErrorResult(result);
-
-                if (errorResult != null)
-                {
-                    return errorResult;
-                }
-
-                return Ok(result);
+                return errorResult;
             }
-            catch (Exception ex)
-            {
-                //FIXME: Need to log these
-                return InternalServerError(ex);
-            }
+
+            return Ok(result);
         }
 
         // FIXME: this definitely looks ugly, need to clean up here
@@ -123,39 +115,24 @@
         [HttpGet]
         public async Task<IHttpActionResult> Profile(string userId = null)
         {
-            try
-            {
-                if (!this.User.Identity.IsAuthenticated && string.IsNullOrEmpty(userId))
-                    return BadRequest("To get a public profile, please provide a valid user Id");
+            if (!this.User.Identity.IsAuthenticated && string.IsNullOrEmpty(userId))
+                return BadRequest("To get a public profile, please provide a valid user Id");
 
-                if (string.IsNullOrWhiteSpace(userId))
-                    userId = this.User.Identity.GetUserId();
+            if (string.IsNullOrWhiteSpace(userId))
+                userId = this.User.Identity.GetUserId();
 
-                var userModel = await accountRepository.FindUserAsModel(userId);
-                if (userModel == null) return NotFound();
+            var userModel = await accountRepository.FindUserAsModel(userId);
+            if (userModel == null) return NotFound();
 
-                if (this.User.Identity.IsAuthenticated)
-                {
-                    if (this.User.IsInRole("Administrator") || this.User.IsInRole("BackOfficeAdmin"))
-                        userModel.IsUserAuthenticated = true;
-                    else if ((this.User.IsInRole("User") || this.User.IsInRole("Asset")) && (this.User.Identity.GetUserId() == userId))
-                        userModel.IsUserAuthenticated = true;
-                }
+            if (this.User.Identity.IsAuthenticated)
+            {
+                if (this.User.IsInRole("Administrator") || this.User.IsInRole("BackOfficeAdmin"))
+                    userModel.IsUserAuthenticated = true;
+                else if ((this.User.IsInRole("User") || this.User.IsInRole("Asset")) && (this.User.Identity.GetUserId() == userId))
+                    userModel.IsUserAuthenticated = true;
+            }
 
-                return Json(userModel);
-            }
-            catch (FormatException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(userModel);
         }
 
         /// <summary>
@@ -182,39 +159,24 @@
         /// <returns>
         /// A paginated set of results for Assigned job against an user id
         /// </returns>
-        
+
         [Authorize(Roles = "Administrator, BackOfficeAdmin, Asset")]
         [HttpGet]
         [Route("{userId?}/jobs")]
         public async Task<IHttpActionResult> GetAssignedJobs(string userId = null, int pageSize = AppConstants.DefaultPageSize, int page = 0, DateTime? dateTimeUpto = null, JobState jobStateUpto = JobState.IN_PROGRESS, SortDirection sortDirection = SortDirection.Descending)
         {
-            try
+            if (!string.IsNullOrWhiteSpace(userId))
             {
-                if (!string.IsNullOrWhiteSpace(userId))
-                {
-                    if (this.User.IsInRole("Asset") && (this.User.Identity.GetUserId() != userId))
-                        return Content(HttpStatusCode.Forbidden, "Accessing Assigned jobs of other Assets is not supported unless you're an admin");
-                }
-                else
-                {
-                    userId = this.User.Identity.GetUserId();
-                }
+                if (this.User.IsInRole("Asset") && (this.User.Identity.GetUserId() != userId))
+                    return Content(HttpStatusCode.Forbidden, "Accessing Assigned jobs of other Assets is not supported unless you're an admin");
+            }
+            else
+            {
+                userId = this.User.Identity.GetUserId();
+            }
 
-                var result = await accountRepository.FindAssignedJobs(userId, page, pageSize, dateTimeUpto, jobStateUpto, sortDirection, this.Request);
-                return Json(result);
-            }
-            catch (FormatException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (InvalidOperationException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            var result = await accountRepository.FindAssignedJobs(userId, page, pageSize, dateTimeUpto, jobStateUpto, sortDirection, this.Request);
+            return Json(result);
         }
 
 
@@ -245,16 +207,10 @@
 
             pageSize = pageSize > AppConstants.MaxPageSize ? AppConstants.MaxPageSize : pageSize = 25;
 
-            try
-            {
-                if (envelope)
-                    return Json(await accountRepository.FindAllEnvelopedAsModel(page, pageSize, this.Request));
-                else
-                    return Json(await accountRepository.FindAllAsModel(page, pageSize));
-            }
-            catch (Exception ex) { return InternalServerError(ex); }
-
-
+            if (envelope)
+                return Json(await accountRepository.FindAllEnvelopedAsModel(page, pageSize, this.Request));
+            else
+                return Json(await accountRepository.FindAllAsModel(page, pageSize));
         }
 
         /// <summary>
@@ -323,29 +279,15 @@
         [Route("profile")]
         public async Task<IHttpActionResult> Update(IdentityProfile model)
         {
-            try
-            {
-                return Json(await accountRepository.Update(model, this.User.Identity.Name));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.Update(model, this.User.Identity.Name));
         }
 
         [Authorize(Roles = "Administrator, BackOfficeAdmin")]
         [HttpPut]
         [Route("profile/{id}")]
-        public async Task<IHttpActionResult> Update( [FromBody]IdentityProfile model, [FromUri]string id)
+        public async Task<IHttpActionResult> Update([FromBody]IdentityProfile model, [FromUri]string id)
         {
-            try
-            {
-                return Json(await accountRepository.UpdateById(model, id));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.UpdateById(model, id));
         }
 
         [Authorize(Roles = "Administrator, BackOfficeAdmin, User, Asset")]
@@ -353,14 +295,7 @@
         [Route("password")]
         public async Task<IHttpActionResult> UpdatePassword(PasswordUpdateModel model)
         {
-            try
-            {
-                return Json(await accountRepository.UpdatePassword(model, this.User.Identity.Name));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.UpdatePassword(model, this.User.Identity.Name));
         }
 
         [Authorize(Roles = "Administrator, BackOfficeAdmin, User, Asset")]
@@ -368,14 +303,7 @@
         [Route("username")]
         public async Task<IHttpActionResult> UpdateUsername([FromUri]string newUsername)
         {
-            try
-            {
-                return Json(await accountRepository.UpdateUsername(newUsername, this.User.Identity.Name));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.UpdateUsername(newUsername, this.User.Identity.Name));
         }
 
         [Authorize(Roles = "Administrator, BackOfficeAdmin")]
@@ -383,14 +311,7 @@
         [Route("username/{userId}")]
         public async Task<IHttpActionResult> UpdateUsername([FromUri]string newUsername, string userId)
         {
-            try
-            {
-                return Json(await accountRepository.UpdateUsernameById(userId, newUsername));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.UpdateUsernameById(userId, newUsername));
         }
 
         // FIXME: Im not sure how this would pan out though, may be would pan out fine or not
@@ -399,14 +320,7 @@
         [Route("username")]
         public async Task<IHttpActionResult> SuggestUserName(string suggestedUserName)
         {
-            try
-            {
-                return Json(await accountRepository.IsUsernameAvailable(suggestedUserName));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+             return Json(await accountRepository.IsUsernameAvailable(suggestedUserName));           
         }
 
         [HttpPut]
@@ -414,14 +328,7 @@
         [Route("contacts")]
         public async Task<IHttpActionResult> UpdateContacts(ContactUpdateModel model)
         {
-            try
-            {
-                return Json(await accountRepository.UpdateContacts(model, this.User.Identity.Name));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return Json(await accountRepository.UpdateContacts(model, this.User.Identity.Name));
         }
 
         /// <summary>
@@ -436,31 +343,19 @@
         [Route("avatar")]
         public async Task<IHttpActionResult> UploadAvatar()
         {
-            try
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
             {
-                if (!Request.Content.IsMimeMultipartContent("form-data"))
-                {
-                    return StatusCode(HttpStatusCode.UnsupportedMediaType);
-                }
-                
-                var result = await accountRepository.UploadAvatar(Request.Content, this.User.Identity.GetUserId());
-
-                if (result != null)
-                {
-                    return Ok(result);
-                }
-
-                return BadRequest();
-
+                return StatusCode(HttpStatusCode.UnsupportedMediaType);
             }
-            catch(NotSupportedException ex)
+
+            var result = await accountRepository.UploadAvatar(Request.Content, this.User.Identity.GetUserId());
+
+            if (result != null)
             {
-                return BadRequest(ex.Message);
+                return Ok(result);
             }
-            catch(Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+
+            return BadRequest();
         }
     }
 }
