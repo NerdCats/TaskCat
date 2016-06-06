ea3270a26e9350ae1a5da235cde568ac15a590ff
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-06 12:01:29 +0600

    Added a little set of properties for the DropPoint class

--- TaskCat/TaskCat.Data/Entity/DropPoint.cs
+++ TaskCat/TaskCat.Data/Entity/DropPoint.cs
@@ -1,7 +1,23 @@
 ﻿namespace TaskCat.Data.Entity
 {
-    public class DropPoint: DbEntity
+    using Model.Geocoding;
+
+    public class DropPoint : DbEntity
     {
-        
+        public DefaultAddress Address { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+
+        public DropPoint()
+        {
+
+        }
+
+        public DropPoint(string userId, string name, DefaultAddress address)
+        {
+            this.Name = name;
+            this.UserId = userId;
+            this.Address = address;
+        }
     }
 }
