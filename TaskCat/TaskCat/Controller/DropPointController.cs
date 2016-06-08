cedd20b2e4522ecdc1d7d32802854779a58b1ec4
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-08 20:22:09 +0600

    #48, Documentation added for DropPointController

--- TaskCat/TaskCat/Controller/DropPointController.cs
+++ TaskCat/TaskCat/Controller/DropPointController.cs
@@ -15,6 +15,7 @@
     using System.Net.Http;
     using System.Threading.Tasks;
     using System.Web.Http;
+    using System.Web.Http.Description;
 
     /// <summary>
     /// Registers and manages drop points for a certain user
@@ -28,8 +29,19 @@
             this.service = service;
         }
 
+        /// <summary>
+        /// Gets a Drop Point based on id and userId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="userId">
+        /// Optional param, needed if an Administrator is actually looking for someone else's DropPoint
+        /// </param>
+        /// <returns>
+        /// A DropPoint matching the query
+        /// </returns>
         [HttpGet]
         [Authorize]
+        [ResponseType(typeof(DropPoint))]
         public async Task<IHttpActionResult> Get(string id, string userId=null)
         {
             var authorizedId = User.Identity.GetUserId();
@@ -53,13 +65,44 @@
             }
         }
 
+        /// <summary>
+        /// Get a list of predefined name suggestions for the drop points
+        /// </summary>
+        /// <returns>
+        /// List of predefined name suggestions for the drop points
+        /// </returns>
+        /// 
         [HttpGet]
         [Route("api/DropPoint/suggestions")]
+        [ResponseType(typeof(List<string>))]
         public IHttpActionResult GetDropPointNameSuggestions()
         {
             return Json(DropPointNameSuggestions.Values);
         }
 
+        /// <summary>
+        /// Gets a list of drop points based on a search on Address and Name 
+        /// </summary>
+        /// <param name="query">
+        /// Text to search in the Address and Name in a Drop Point
+        /// </param>
+        /// <param name="userId">
+        /// Optional, this field is populated automatically from authentication header if not provided
+        /// If an administrator wants to search in one specific user's droppoint list he would have to use 
+        /// this field
+        /// </param>
+        /// <param name="pageSize">
+        /// page size to return result in
+        /// </param>
+        /// <param name="page">
+        /// page number 
+        /// </param>
+        /// <param name="envelope">
+        /// Defines whether the result should be sent back as a paged result or not
+        /// </param>
+        /// <returns>
+        /// Returns either a <see cref="PageEnvelope{DropPoint}"/> or a <see cref="List{DropPoint}"/>
+        /// </returns>
         [HttpGet]
         [Authorize]
         [Route("api/DropPoint/search")]
@@ -90,6 +133,23 @@
             return Json(queryResult);
         }
 
+        /// <summary>
+        /// Odata endpoint for only Administrator and BackendOfficeAdmins to execute queries needed 
+        /// for components that needs a comprehensive list
+        /// </summary>
+        /// <param name="pageSize">
+        /// page size
+        /// </param>
+        /// <param name="page">
+        /// page number
+        /// </param>
+        /// <param name="envelope">
+        /// Defines whether the result should be sent back as a paged result or not
+        /// </param>
+        /// <returns>
+        /// Returns either a <see cref="PageEnvelope{DropPoint}"/> or a <see cref="List{DropPoint}"/>
+        /// </returns>
+        /// 
         [HttpGet]
         [Route("api/DropPoint/odata")]
         [Authorize(Roles = "Administrator, BackOfficeAdmin")]
@@ -125,8 +185,18 @@
             return Json(queryResult);
         }
 
+        /// <summary>
+        /// Submit a new drop point 
+        /// </summary>
+        /// <param name="value">
+        /// DropPoint Value to be submitted
+        /// </param>
+        /// <returns>
+        /// The newly created drop point
+        /// </returns>
         [HttpPost]
         [Authorize]
+        [ResponseType(typeof(DropPoint))]
         public async Task<IHttpActionResult> Post([FromBody]DropPoint value)
         {
             if (!ModelState.IsValid)
@@ -156,8 +226,19 @@
             return (this.User.IsInRole(RoleNames.ROLE_ADMINISTRATOR) || this.User.IsInRole(RoleNames.ROLE_BACKOFFICEADMIN));
         }
 
+        /// <summary>
+        /// Update a certain drop point
+        /// </summary>
+        /// <param name="value">
+        /// Drop point to be updated, only an administrator can save change any
+        /// users drop point userId field to other userId field
+        /// </param>
+        /// <returns>
+        /// Returns the modified drop point
+        /// </returns>
         [HttpPut]
         [Authorize]
+        [ResponseType(typeof(DropPoint))]
         public async Task<IHttpActionResult> Put(DropPoint value)
         {
             if (!ModelState.IsValid)
@@ -185,8 +266,21 @@
             
         }
 
+        /// <summary>
+        /// Deletes a drop point based on id and optionally user id
+        /// </summary>
+        /// <param name="id">
+        /// Sepcific drop point id to be deleted
+        /// </param>
+        /// <param name="userId">
+        /// Optional, if an Administrator wants to delete a specific id of an user, he can use this field
+        /// </param>
+        /// <returns>
+        /// Deleted drop point
+        /// </returns>
         [HttpDelete]
         [Authorize]
+        [ResponseType(typeof(DropPoint))]
         public async Task<IHttpActionResult> Delete(string id, string userId = null)
         {
             var authorizedId = User.Identity.GetUserId();
