4640f085ba83d5a9c495223aee30462004249ffe
Swagata Prateek <thehoneymad@hotmail.com>
2016-08-05 16:26:44 +0200

    Some resharper sharpening

--- TaskCat/TaskCat/Lib/Job/JobRepository.cs
+++ TaskCat/TaskCat/Lib/Job/JobRepository.cs
@@ -23,41 +23,41 @@
 
     public class JobRepository : IJobRepository
     {
-        private IJobManager manager;
-        private AccountManager accountManager; // FIXME: When a full fledged assetManager comes up this should be replaced by that
+        private IJobManager _manager;
+        private AccountManager _accountManager; // FIXME: When a full fledged assetManager comes up this should be replaced by that
 
         public JobRepository(IJobManager manager, AccountManager accountManager)
         {
-            this.manager = manager;
-            this.accountManager = accountManager;
+            this._manager = manager;
+            this._accountManager = accountManager;
         }
 
         public async Task<Job> GetJob(string id)
         {
-            return await manager.GetJob(id);
+            return await _manager.GetJob(id);
         }
 
         public async Task<IQueryable<Job>> GetJobs()
         {
-            return await manager.GetJobs();
+            return await _manager.GetJobs();
         }
 
         public async Task<Job> GetJobByHrid(string hrid)
         {
-            return await manager.GetJobByHRID(hrid);
+            return await _manager.GetJobByHRID(hrid);
         }
 
         public async Task<IEnumerable<Job>> GetJobs(string type, int page, int pageSize)
         {
             if (page < 0)
                 throw new ArgumentException("Invalid page index provided");
-            return await manager.GetJobs(type, page * pageSize, pageSize);
+            return await _manager.GetJobs(type, page * pageSize, pageSize);
         }
 
         public async Task<PageEnvelope<Job>> GetJobsEnveloped(string type, int page, int pageSize, HttpRequestMessage request)
         {
             var data = await GetJobs(type, page, pageSize);
-            var total = await manager.GetTotalJobCount();
+            var total = await _manager.GetTotalJobCount();
 
             return new PageEnvelope<Job>(total, page, pageSize, AppConstants.DefaultApiRoute, data, request, new Dictionary<string, string>() { ["type"] = type });
         }
@@ -69,7 +69,7 @@
 
         public async Task<ReplaceOneResult> UpdateJob(Job job)
         {
-            return await manager.UpdateJob(job);
+            return await _manager.UpdateJob(job);
         }
 
         public async Task<ReplaceOneResult> UpdateOrder(Job job, OrderModel orderModel)
@@ -128,28 +128,21 @@
 
         public async Task<bool> ResolveAssetRef(JsonPatchDocument<JobTask> taskPatch)
         {
-            var AssetRefReplaceOp = taskPatch.Operations.FirstOrDefault(x => x.op == "replace" && x.path == "/AssetRef");
-            if (AssetRefReplaceOp != null && AssetRefReplaceOp.value.GetType() == typeof(string))
-            {
-                // INFO: Now we need to actually fetch the asset and get shit done
-                var asset = await accountManager.FindAsByIdAsync<Data.Entity.Identity.Asset>(AssetRefReplaceOp.value.ToString());
-                if (asset == null) return false;
-                var assetModel = new AssetModel(asset);
-                AssetRefReplaceOp.path = "/Asset";
-                AssetRefReplaceOp.value = assetModel as object;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
+            var assetRefReplaceOp = taskPatch.Operations.FirstOrDefault(x => x.op == "replace" && x.path == "/AssetRef");
+            if (!(assetRefReplaceOp?.value is string)) return false;
+            // INFO: Now we need to actually fetch the asset and get shit done
+            var asset = await _accountManager.FindAsByIdAsync<Data.Entity.Identity.Asset>(assetRefReplaceOp.value.ToString());
+            if (asset == null) return false;
+            var assetModel = new AssetModel(asset);
+            assetRefReplaceOp.path = "/Asset";
+            assetRefReplaceOp.value = assetModel;
+            return true;
         }
 
         public async Task<ReplaceOneResult> Claim(string jobId, string userId)
         {
             var job = await GetJob(jobId);
-            var adminUser = await accountManager.FindByIdAsync(userId);
+            var adminUser = await _accountManager.FindByIdAsync(userId);
             var userModel = new UserModel(adminUser);
             job.JobServedBy = userModel;
 
