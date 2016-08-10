56696ab1ab6e830952e377bb7bdd2531ac47ef84
Swagata Prateek <s.prateek@hotmail.com>
2016-08-10 21:28:56 +0200

    Added data annotation to make sure the Classified delivery order requirements are fullfilled

--- TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
+++ TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
@@ -1,9 +1,13 @@
 ﻿namespace TaskCat.Data.Model.Order
 {
     using Person;
+    using System.ComponentModel.DataAnnotations;
+
     public class ClassifiedDeliveryOrder : DeliveryOrder
     {
+        [Required(ErrorMessage = "Seller info not provieded", ErrorMessageResourceType = typeof(PersonInfo))]
         public PersonInfo SellerInfo { get; set; }
+        [Required(ErrorMessage = "Buyer info not provieded", ErrorMessageResourceType = typeof(PersonInfo))]
         public PersonInfo BuyerInfo { get; set; }
 
         public ClassifiedDeliveryOrder(string name = null, string type = OrderTypes.ClassifiedDelivery)
