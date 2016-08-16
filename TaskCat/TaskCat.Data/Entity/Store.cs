ee25e7ca5d9a6d73cdfa92e7a93214d28344af64
Swagata Prateek <thehoneymad@hotmail.com>
2016-08-16 23:41:25 +0200

    Added a basic store class

---
+++ TaskCat/TaskCat.Data/Entity/Store.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskCat.Data.Entity
+{
+    /// <summary>
+    /// Represents a store
+    /// </summary>
+    public class Store : DbEntity
+    {
+        /// <summary>
+        /// Gets or sets the store name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the store URL
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the enterprise user id associated with it
+        /// </summary>
+        public string EnterpriseUserId { get; set; }
+    }
+}
