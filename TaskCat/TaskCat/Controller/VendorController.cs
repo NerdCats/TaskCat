55e8420ddc394bfe32853addd024ab258fe8bfdc
Swagata Prateek <thehoneymad@hotmail.com>
2016-08-17 23:23:49 +0200

    Started adding changes so that an enterprise user can subscribe to be a vendor

---
+++ TaskCat/TaskCat/Controller/VendorController.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskCat.Controller
+{
+    using Lib.Vendor;
+    using Microsoft.AspNet.Identity;
+    using System.Web.Http;
+    using System;
+    using Lib.Utility;
+    using System.Threading.Tasks;
+
+    [RoutePrefix("api/Vendor")]
+    public class VendorController : ApiController
+    {
+        private IVendorService service;
+
+        public VendorController(IVendorService service)
+        {
+            this.service = service;
+        }
+
+        [Authorize(Roles = "Administrator, Enterprise, BackOfficeAdmin")]
+        [Route("Subscribe")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Subscribe(string userId = null)
+        {
+            var authorizedId = User.Identity.GetUserId();
+            if (!User.IsAdmin() && !string.IsNullOrEmpty(userId) && authorizedId != userId)
+                throw new InvalidOperationException($"User {authorizedId} is not authorized to subscribe for vendorship for user {userId}");
+
+            bool result = await service.Subscribe(userId);
+
+            if (result)
+                return Ok();
+            return StatusCode(System.Net.HttpStatusCode.Gone);          
+        }
+    }
+}
---
+++ TaskCat/TaskCat/Lib/Vendor/IVendorService.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskCat.Lib.Vendor
+{
+    using System.Threading.Tasks;
+
+    public interface IVendorService
+    {
+        Task<bool> Subscribe(string userId);
+    }
+}
