e764e57b98b5cd5f70512613c61e3ec10d79c6f6
Swagata Prateek <thehoneymad@hotmail.com>
2016-08-24 22:53:58 +0200

    Started adding profit sharing strategies

---
+++ TaskCat/TaskCat.Data/Model/Vendor/FlatRateProfitSharingPreference.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskCat.Data.Model.Vendor
+{
+    public class FlatRateProfitSharingPreference : IProfitSharingPreference
+    {
+        public decimal FlatRate { get; set; } = 50;
+        public ProfitSharingMethod Method
+        {
+            get { return ProfitSharingMethod.FLAT_RATE; }
+        }
+
+        public decimal Calculate(decimal totalPrice) => FlatRate;
+    }
+}
---
+++ TaskCat/TaskCat.Data/Model/Vendor/IProfitSharingPreference.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskCat.Data.Model.Vendor
+{
+    public interface IProfitSharingPreference
+    {
+        ProfitSharingMethod Method { get; }
+
+        decimal Calculate(decimal totalPrice);
+    }
+}
