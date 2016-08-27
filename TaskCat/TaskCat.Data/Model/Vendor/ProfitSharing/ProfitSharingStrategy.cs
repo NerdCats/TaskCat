5c0ac1ef79313f537a5355c7f49b63df78ee0e06
Swagata Prateek <thehoneymad@hotmail.com>
2016-08-27 16:09:24 +0200

    Added BsonKnownTypes for ProfitSharingStrategies

--- TaskCat/TaskCat.Data/Model/Vendor/ProfitSharing/ProfitSharingStrategy.cs
+++ TaskCat/TaskCat.Data/Model/Vendor/ProfitSharing/ProfitSharingStrategy.cs
@@ -2,15 +2,11 @@
 {
     using MongoDB.Bson.Serialization.Attributes;
 
-    [BsonKnownTypes(typeof(FlatRateStrategy))]
+    [BsonKnownTypes(typeof(FlatRateStrategy), typeof(PricePercentageStrategy))]
     public abstract class ProfitSharingStrategy
     {
         internal abstract protected ProfitSharingMethod Method { get; }
 
         internal protected abstract decimal Calculate(decimal totalPrice);
-
-        public ProfitSharingStrategy()
-        {
-        }
     }
 }
