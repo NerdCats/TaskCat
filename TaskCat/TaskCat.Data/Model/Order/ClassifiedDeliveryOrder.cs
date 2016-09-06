7ed6f2cc23f5bf41b96f0c74befad880fecae757
Swagata Prateek <thehoneymad@hotmail.com>
2016-09-06 20:20:26 +0200

    Bugfix: Fixed the missing ErrorMessage missing issue

--- TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
+++ TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
@@ -5,9 +5,9 @@
 
     public class ClassifiedDeliveryOrder : DeliveryOrder
     {
-        [Required(ErrorMessage = "Seller info not provieded", ErrorMessageResourceType = typeof(PersonInfo))]
+        [Required(ErrorMessage = "Seller info not provieded", ErrorMessageResourceType = typeof(PersonInfo), ErrorMessageResourceName = nameof(SellerInfo))]
         public PersonInfo SellerInfo { get; set; }
-        [Required(ErrorMessage = "Buyer info not provieded", ErrorMessageResourceType = typeof(PersonInfo))]
+        [Required(ErrorMessage = "Buyer info not provieded", ErrorMessageResourceType = typeof(PersonInfo), ErrorMessageResourceName = nameof(BuyerInfo))]
         public PersonInfo BuyerInfo { get; set; }
 
         public ClassifiedDeliveryOrder(string name = null, string type = OrderTypes.ClassifiedDelivery)
