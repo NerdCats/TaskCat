d1f5ad5569dc5a6d7d746f3b8e0f642ab405ad50
Swagata Prateek <thehoneymad@hotmail.com>
2016-09-28 20:29:15 +0200

    GFETCH-134: Fixed, redundant qualifiers

--- TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
+++ TaskCat/TaskCat.Data/Model/Order/ClassifiedDeliveryOrder.cs
@@ -5,10 +5,10 @@
 
     public class ClassifiedDeliveryOrder : DeliveryOrder
     {
-        [Required(ErrorMessage = "Seller info not provieded", ErrorMessageResourceType = typeof(PersonInfo), ErrorMessageResourceName = nameof(SellerInfo))]
+        [Required(ErrorMessage = "Seller info not provided")]
         public PersonInfo SellerInfo { get; set; }
 
-        [Required(ErrorMessage = "Buyer info not provieded", ErrorMessageResourceType = typeof(PersonInfo), ErrorMessageResourceName = nameof(BuyerInfo))]
+        [Required(ErrorMessage = "Buyer info not provided")]
         public PersonInfo BuyerInfo { get; set; }
 
         public ClassifiedDeliveryOrder(string name = null, string type = OrderTypes.ClassifiedDelivery)
