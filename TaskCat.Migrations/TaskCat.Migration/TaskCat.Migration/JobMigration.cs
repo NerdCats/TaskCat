70c60d14e7a4b959f6722e51a47b61fc0f89ce78
Swagata Prateek <thehoneymad@hotmail.com>
2016-10-05 18:04:38 +0200

    Added Mongo migrations to TaskCat

---
+++ TaskCat.Migrations/TaskCat.Migration/TaskCat.Migration/JobMigration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoMigrations;
+
+namespace TaskCat.Migration
+{
+    public class JobMigration: CollectionMigration
+    {
+        public JobMigration(MigrationVersion version, string collectionName) : base(version, collectionName)
+        {
+        }
+
+        public override void UpdateDocument(IMongoCollection<BsonDocument> collection, BsonDocument document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
