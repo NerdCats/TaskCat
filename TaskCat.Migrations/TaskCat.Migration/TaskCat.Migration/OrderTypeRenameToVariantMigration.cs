0b7c5acce87263e65be8d545b3f020e1a162377f
Swagata Prateek <thehoneymad@hotmail.com>
2016-10-05 19:11:01 +0200

    Did the full migration in an atomic manner

--- TaskCat.Migrations/TaskCat.Migration/TaskCat.Migration/OrderTypeRenameToVariantMigration.cs
+++ TaskCat.Migrations/TaskCat.Migration/TaskCat.Migration/OrderTypeRenameToVariantMigration.cs
@@ -4,23 +4,23 @@
     using MongoDB.Driver;
     using MongoMigrations;
 
-    public class OrderTypeRenameToVariantMigration : CollectionMigration
+    public class OrderTypeRenameToVariantMigration : Migration
     {
-        public OrderTypeRenameToVariantMigration() : base("1.0.0", "Jobs")
+        public OrderTypeRenameToVariantMigration() : base("1.0.0")
         {
             Description = "Rename Order.PayloadType to Order.Variant";
-        }
-
-        public override void UpdateDocument(IMongoCollection<BsonDocument> collection, BsonDocument document)
-        {
-            UpdateDefinition<BsonDocument> update = GetRenameDefinition();
-            var result = collection.UpdateOne(x => true, update);
         }
 
         public UpdateDefinition<BsonDocument> GetRenameDefinition()
         {
             return Builders<BsonDocument>.Update
                 .Rename("Order.PayloadType", "Order.Variant");
+        }
+
+        public override void Update()
+        {
+            UpdateDefinition<BsonDocument> update = GetRenameDefinition();
+            Database.GetCollection<BsonDocument>("Jobs").UpdateMany(x => true, update);
         }
     }
 }
