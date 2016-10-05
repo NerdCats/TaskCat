ddaecfeab8a9a4193c7620fbe3917bf9fc144164
Swagata Prateek <thehoneymad@hotmail.com>
2016-10-05 19:00:02 +0200

    GFETCH-161: Added migration project and necessary submodules, tested the migration, works :)

---
+++ TaskCat.Migrations/TaskCat.Migration/TaskCat.Migration.Test/MigrationTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskCat.Migration.Test
+{
+    using NUnit.Framework;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+    using MongoDB.Bson.Serialization.Serializers;
+
+    [TestFixture]
+    public class MigrationTests
+    {
+        [Test]
+        public void MigrationToRenamePayloadTypeToVariant_HasPayloadType_RenamesToVariant()
+        {
+            var updateFilter = new BsonDocument
+            {
+                { "$rename", new BsonDocument
+                    {
+                        {"Order.PayloadType", "Order.Variant" }
+                    }
+                }
+            };       
+
+            var migration = new OrderTypeRenameToVariantMigration();
+            var updateDef = migration.GetRenameDefinition();
+            
+            var result = updateDef.Render(new BsonDocumentSerializer(), new BsonSerializerRegistry());
+            Assert.AreEqual(updateFilter, result);
+        }
+    }
+}
---
+++ TaskCat.Migrations/TaskCat.Migration/TaskCat.Migration/OrderTypeRenameToVariantMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskCat.Migration
+{
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using MongoMigrations;
+
+    public class OrderTypeRenameToVariantMigration : CollectionMigration
+    {
+        public OrderTypeRenameToVariantMigration() : base("1.0.0", "Jobs")
+        {
+            Description = "Rename Order.PayloadType to Order.Variant";
+        }
+
+        public override void UpdateDocument(IMongoCollection<BsonDocument> collection, BsonDocument document)
+        {
+            UpdateDefinition<BsonDocument> update = GetRenameDefinition();
+            var result = collection.UpdateOne(x => true, update);
+        }
+
+        public UpdateDefinition<BsonDocument> GetRenameDefinition()
+        {
+            return Builders<BsonDocument>.Update
+                .Rename("Order.PayloadType", "Order.Variant");
+        }
+    }
+}
