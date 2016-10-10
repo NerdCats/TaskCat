1b7b80ef6c279f4c3fd4bb79b7ea01e4665b58d7
Swagata Prateek <thehoneymad@hotmail.com>
2016-10-10 08:59:33 +0200

    GFETCH-174: Fixed wrong attribute routing headers

--- TaskCat/TaskCat/Controller/StoreController.cs
+++ TaskCat/TaskCat/Controller/StoreController.cs
@@ -29,7 +29,7 @@
         }
 
         [HttpGet]
-        [Route("api/ProductCategory/odata")]
+        [Route("api/Store/odata")]
         public async Task<IHttpActionResult> Get(int pageSize = AppConstants.DefaultPageSize, int page = 0, bool envelope = true)
         {
             if (pageSize == 0)
