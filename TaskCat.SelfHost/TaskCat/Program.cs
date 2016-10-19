6680cef1674ee491973181a2ce0be259f1f68579
Swagata Prateek <thehoneymad@hotmail.com>
2016-10-19 17:01:31 +0200

    made sure the new windows service works.

---
+++ TaskCat.SelfHost/TaskCat/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dichotomy;
+using Dichotomy.Configuration;
+using Dichotomy.Helpers;
+
+namespace TaskCat
+{
+    public static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            ServiceManager.Initialize("TaskCatApiService", "TaskCat Api Service", "This is the api service of TaskCat");
+
+            var config = new ConfigurationOptions("--");
+            var runner = new Runner(new TaskCatApiService(), config);
+            runner.Run();
+        }
+    }
+}
---
+++ TaskCat.SelfHost/TaskCat/TaskCatApiService.cs
@@ -0,0 +1,46 @@
+﻿namespace TaskCat
+{
+    using System;
+    using Dichotomy;
+    using Microsoft.Owin.Hosting;
+    using Lib.Constants;
+    using Its.Configuration;
+    using App.Settings;
+
+    public class TaskCatApiService : IDichotomyService
+    {
+        private string listeningAddress;
+        private IDisposable webApp;
+
+        public TaskCatApiService()
+        {
+           this.listeningAddress = string.IsNullOrWhiteSpace(Settings.Get<ClientSettings>().HostingAddress)
+                ? AppConstants.DefaultHostingAddress : Settings.Get<ClientSettings>().HostingAddress;
+        }
+
+        public void Dispose()
+        {
+            if (webApp != null)
+            {
+                webApp.Dispose();
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting TaskCat Api Service");
+
+            this.webApp = WebApp.Start(listeningAddress, appBuilder => Startup.ConfigureApp(appBuilder));
+
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Hosted TaskCat on {listeningAddress}");
+            Console.ForegroundColor = oldColor;
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping TaskCat Api Service");
+        }
+    }
+}
