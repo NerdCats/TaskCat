3b66432fc294526d3b175f56305a57de18973ac9
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-15 00:36:20 +0100

    GFETCH-61: Initial commit, decoupling authentication process to authentication microservice

---
+++ TaskCat.SelfHost/TaskCat.Auth/Lib/TaskCatOAuthProvider.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Owin.Security.OAuth;
+
+namespace TaskCat.Auth.Lib
+{
+    public class TaskCatOAuthProvider: OAuthAuthorizationServerProvider
+    {
+
+    }
+}
---
+++ TaskCat.SelfHost/TaskCat.Auth/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+namespace TaskCat.Auth
+{
+    public class Startup
+    {
+        public void ConfigureApp(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://jwtauthzsrv.azurewebsites.net")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+    }
+}
