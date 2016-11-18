d0cb9060bd0beb9ceafe8f678a5013007cfcef78
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-18 23:56:48 +0100

    GFETCH-241: Last little change to make sure the welcome page is right

--- TaskCat.SelfHost/TaskCat.Account/Startup.cs
+++ TaskCat.SelfHost/TaskCat.Account/Startup.cs
@@ -59,7 +59,7 @@
             app.Run(context =>
             {
                 context.Response.ContentType = "text/plain";
-                return context.Response.WriteAsync(string.Format($"Welcome to TaskCat Auth '{version}', proudly baked by NerdCats"));
+                return context.Response.WriteAsync(string.Format($"Welcome to TaskCat Account '{version}', proudly baked by NerdCats"));
             });
         }
 
