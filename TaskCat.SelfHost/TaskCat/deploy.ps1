5f585eaa51d86daf16a290f631988d1a1f59b4ad
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-18 19:35:34 +0100

    GFETCH-61: Changes to deploy.ps1

--- TaskCat.SelfHost/TaskCat/deploy.ps1
+++ TaskCat.SelfHost/TaskCat/deploy.ps1
@@ -1,2 +1,5 @@
-﻿& 'TaskCat.exe --uninstall'
-& 'TaskCat.exe --install'
+﻿$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$scriptPath = Join-Path $executingScriptDirectory "TaskCat.exe"
+
+Invoke-Expression "$scriptPath --uninstall"
+Invoke-Expression "$scriptPath --install"
