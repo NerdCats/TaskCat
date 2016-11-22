5263286b229223c278fffeac4442bea658341c96
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-22 18:40:19 +0100

    GFETCH-243: Added job activity reference

---
+++ TaskCat.SelfHost/TaskCat.Data/Entity/JobActivity.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskCat.Data.Entity
+{
+    public class JobActivity : DbEntity
+    {
+        public string JobId { get; set; }
+        public string  Operation { get; set; }
+        public ReferenceActivity Reference { get; set; }
+        public string Value { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class ReferenceActivity
+    {
+        public string Id { get; set; }
+        public string EntityType { get; set; }
+    }
+}
