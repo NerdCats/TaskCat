e350cf7fd5c09b30a88dcde1f95f39baa2b73fdc
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-22 21:32:27 +0100

    GFETCH-264: Added JobActivity class changes

--- TaskCat.SelfHost/TaskCat.Data/Entity/JobActivity.cs
+++ TaskCat.SelfHost/TaskCat.Data/Entity/JobActivity.cs
@@ -1,17 +1,34 @@
 ﻿namespace TaskCat.Data.Entity
 {
+    using System;
     public class JobActivity : DbEntity
     {
         public string JobId { get; set; }
         public string  Operation { get; set; }
-        public ReferenceActivity Reference { get; set; }
         public string Value { get; set; }
         public string Path { get; set; }
+
+        public ReferenceActivity Reference { get; set; }
+        public ReferenceUser User { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public JobActivity()
+        {
+            TimeStamp = DateTime.UtcNow;
+        }
     }
 
     public class ReferenceActivity
     {
         public string Id { get; set; }
         public string EntityType { get; set; }
+    }
+
+    public class ReferenceUser
+    {
+        public string Username { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
