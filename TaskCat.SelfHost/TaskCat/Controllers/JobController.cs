878889f57182bac95b1ad97fc7ba52a9a062526d
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-24 01:45:47 +0100

    GFETCH-264: Little refactor, now need to write some unit tests

--- TaskCat.SelfHost/TaskCat/Controllers/JobController.cs
+++ TaskCat.SelfHost/TaskCat/Controllers/JobController.cs
@@ -367,14 +367,7 @@ namespace TaskCat.Controllers
             Task.Factory.StartNew(()=> {
                 foreach (var op in taskPatch.Operations)
                 {
-                    var activity = new JobActivity(result.UpdatedValue, JobActivityOperatioNames.Update, currentUser)
-                    {
-                        Reference = new ReferenceActivity(taskId, updatedTask.Type),
-                        Path = $"Tasks[{taskId}].{op.path.Substring(1)}",
-                        Value = op.value,
-                        ActionText = $"{currentUser.DisplayName} updated {op.path.Substring(1)} of {updatedTask.Type} task of job {result.UpdatedValue.HRID} to {op.value}"
-                    };
-
+                    var activity = new JobActivity(result.UpdatedValue, JobActivityOperatioNames.Update, $"Tasks[{taskId}].{op.path.Substring(1)}", currentUser, new ReferenceActivity(taskId, updatedTask.Type));
                     activitySubject.OnNext(activity);
                 }
             });
