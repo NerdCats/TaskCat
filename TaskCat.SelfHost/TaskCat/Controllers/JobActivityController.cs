ba56a8635ca2bf259b08675d6b3f3efb2a3ef3b8
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-24 22:24:11 +0100

    GFETCH-264: Final set of changes for job activity fetching

--- TaskCat.SelfHost/TaskCat/Controllers/JobActivityController.cs
+++ TaskCat.SelfHost/TaskCat/Controllers/JobActivityController.cs
@@ -11,6 +11,9 @@
     using Data.Entity;
     using Lib.Constants;
     using Common.Utility.Odata;
+    using Common.Lib.Utility;
+    using Data.Entity.Identity;
+    using Microsoft.AspNet.Identity;
 
     /// <summary>
     /// Controller for job activities.
@@ -32,13 +35,25 @@
         /// Default Odata route to get job activities
         /// </summary>
         /// <returns></returns>
-        //[Authorize(Roles = "Administrator, BackOfficeAdmin")]
+        [Authorize]
         [ResponseType(typeof(PageEnvelope<JobActivity>))]
         [HttpGet]
         [TaskCatOdataRoute(maxPageSize: AppConstants.MaxPageSize)]
         public async Task<IHttpActionResult> GetJobActivityFeed()
         {
-            IQueryable<JobActivity> activities = dbContext.JobActivityCollection.AsQueryable();
+            IQueryable<JobActivity> activities = null;
+            if (!User.IsAdmin())
+            {
+                if (User.IsInRole(RoleNames.ROLE_ASSET))
+                    activities = dbContext.JobActivityCollection.AsQueryable().Where(x => x.ByUser.Id == this.User.Identity.GetUserId());
+                else
+                    activities = dbContext.JobActivityCollection.AsQueryable().Where(x => x.ForUser.Id == this.User.Identity.GetUserId());
+            }
+            else
+            {
+                activities = dbContext.JobActivityCollection.AsQueryable();
+            }
+            
             var odataResult = await activities.ToOdataResponse(Request, AppConstants.JobActivityOdataRoute);
             return Ok(odataResult);
         }
