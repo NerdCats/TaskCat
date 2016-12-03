82a992448f10922b60acc3961fd0d3088ff6574b
Swagata Prateek <thehoneymad@hotmail.com>
2016-12-03 16:54:23 +0100

    GFETCH-296: Setting up ElasticSearch for Job search

---
+++ TaskCat.SelfHost/TaskCat.Common/Search/SearchContext.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskCat.Common.Search
+{
+    using Nest;
+    using System;
+    using Settings;
+    using AppSettings = Its.Configuration.Settings;
+
+    public class SearchContext : ISearchContext
+    {
+        public ElasticClient Client { get; set; }
+
+        public SearchContext()
+        {
+            InitiateConnection();
+        }
+
+        private void InitiateConnection()
+        {
+            var connectionUrl = new Uri(AppSettings.Get<ElasticSearchSettings>().ConnectionString);
+            var settings = new ConnectionSettings(connectionUrl);
+            this.Client = new ElasticClient(settings);
+        }
+    }
+}
---
+++ TaskCat.SelfHost/TaskCat/Lib/Job/JobSearchIndexService.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskCat.Lib.Job
+{
+    using System;
+    using Data.Entity;
+
+    public class JobSearchIndexService
+    {
+        public JobSearchIndexService(IObservable<Job> jobIndexSource)
+        {
+
+        }
+    }
+}
