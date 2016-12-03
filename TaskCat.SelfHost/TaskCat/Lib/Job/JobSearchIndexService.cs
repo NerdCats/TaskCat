666ae64b11ca2077f31d7bdfdd6810a916dc3f80
Swagata Prateek <thehoneymad@hotmail.com>
2016-12-03 23:27:32 +0100

    GFETCH-296: Added a basic job indexing service

--- TaskCat.SelfHost/TaskCat/Lib/Job/JobSearchIndexService.cs
+++ TaskCat.SelfHost/TaskCat/Lib/Job/JobSearchIndexService.cs
@@ -3,16 +3,38 @@
     using System;
     using Data.Entity;
     using Common.Search;
+    using System.Reactive.Linq;
 
     public class JobSearchIndexService
     {
         private ISearchContext context;
         private IObservable<Job> jobIndexSource;
 
-        public JobSearchIndexService(IObservable<Job> jobIndexSource, ISearchContext context)
+        public JobSearchIndexService(ISearchContext context, IObservable<Job> jobIndexSource)
         {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (jobIndexSource == null)
+                throw new ArgumentNullException(nameof(jobIndexSource));
+
             this.jobIndexSource = jobIndexSource;
             this.context = context;
+
+            this.jobIndexSource
+                .Subscribe(OnNext, OnError);
+        }
+
+        private void OnNext(Job job)
+        {
+            // TODO: Just saving it on the database for now
+            // Log activity here
+            context.Client.Index(job, idx => idx.Index(nameof(Job)));     
+        }
+
+        private void OnError(Exception exception)
+        {
+            Console.WriteLine(exception);
+            // TODO: Log the exception here
         }
     }
 }
