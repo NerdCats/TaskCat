5cc6664897c57e91390803322a362551bd65aefa
riyad <ririyad.291@gmail.com>
2016-12-05 14:20:48 +0600

    GFETCH-298 Removed the Required annotation

--- TaskCat.SelfHost/TaskCat.Data/Model/OrderModel.cs
+++ TaskCat.SelfHost/TaskCat.Data/Model/OrderModel.cs
@@ -124,7 +124,6 @@
         /// <summary>
         /// Added a reference invoice id for every order
         /// </summary>
-        [Required(ErrorMessage ="Please give an invoice id")]
         public string ReferenceInvoiceId { get; set; }
 
         public OrderDetails OrderCart { get; set; }
