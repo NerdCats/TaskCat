b52dd3f15089025eca49ed7145c306f82c43ebd5
Swagata Prateek <thehoneymad@hotmail.com>
2017-01-05 06:12:28 +0100

    GFETCH-325: Added locality collection to the dbContext

---
+++ TaskCat.SelfHost/TaskCat.Job/LocalityService.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskCat.Job
+{
+    using Common.Db;
+    using System;
+    using System.Threading.Tasks;
+
+    public class LocalityService : ILocalityService
+    {
+        public LocalityService(IDbContext context)
+        {
+            this.context = context;
+        }
+        public Task RefreshLocalities()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
