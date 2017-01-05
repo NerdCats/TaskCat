bb7dd07a919cdefc7fa49b1405c9152d69fbc759
Swagata Prateek <thehoneymad@hotmail.com>
2017-01-05 19:36:56 +0100

    GFETCH-325: Locality controller added

--- TaskCat.SelfHost/TaskCat/Controllers/LocalityController.cs
+++ TaskCat.SelfHost/TaskCat/Controllers/LocalityController.cs
@@ -1,33 +1,47 @@
 ﻿namespace TaskCat.Controllers
 {
     using Job;
+    using Common.Model.Pagination;
     using Common.Utility.ActionFilter;
     using Common.Utility.Odata;
+    using Data.Entity;
     using Lib.Constants;
     using MongoDB.Driver;
+    using System.Web.Http.Description;
     using System.Threading.Tasks;
     using System.Web.Http;
 
     public class LocalityController : ApiController
     {
-        private ILocalityService service;
+        private readonly ILocalityService _service;
 
+        /// <summary>
+        /// Creates a single instance of LocalityController.
+        /// It's a controller for handling localities used in jobs.
+        /// </summary>
+        /// <param name="service"></param>
         public LocalityController(ILocalityService service)
         {
-            this.service = service;
+            this._service = service;
         }
 
+        /// <summary>
+        /// Get the current set of localities used in jobs. 
+        /// </summary>
+        /// <param name="refresh">Set to true for refreshing the list of localities. Default is false.</param>
+        /// <returns>A list of </returns>
+        [Authorize]
+        [ResponseType(typeof(PageEnvelope<Locality>))]
         [HttpGet]
         [TaskCatOdataRoute(maxPageSize: AppConstants.MaxPageSize)]
         public async Task<IHttpActionResult> GetLocalities(bool refresh = false)
         {
             if (refresh)
             {
-                await service.RefreshLocalities();
-                return Ok();
+                await _service.RefreshLocalities();
             }
 
-            var localities = service.Collection.AsQueryable();
+            var localities = _service.Collection.AsQueryable();
             var result = await localities.ToOdataResponse(Request, AppConstants.DefaultApiRoute);
             return Ok(result);
         }
