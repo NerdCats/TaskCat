a367ca6a96e9a863af382618334fa782c4ebe87c
thehoneymad <thehoneymad@hotmail.com>
2017-01-25 11:15:30 +0000

    GFETCH-338: Basics of a job extension

--- TaskCat.SelfHost/TaskCat.Data/Model/JobTaskState.cs
+++ TaskCat.SelfHost/TaskCat.Data/Model/JobTaskState.cs
@@ -2,11 +2,31 @@
 {
     public enum JobTaskState
     {
+        /// <summary>
+        /// Task has not started yet
+        /// </summary>
         PENDING = 1,
+        /// <summary>
+        /// Task is in progress
+        /// </summary>
         IN_PROGRESS = 2,
+        /// <summary>
+        /// Task is completed
+        /// </summary>
         COMPLETED = 4,
+        /// <summary>
+        /// Task is cancelled, no default operation to follow.
+        /// Incerementing attempt should be default.
+        /// </summary>
         CANCELLED = 8,
+        /// <summary>
+        /// Task is returned, default operation is to return product to the sender.
+        /// </summary>
         RETURNED = 16,
+        /// <summary>
+        /// Task has failed, deafult operation is to increment the attempt.
+        /// Can be customized for an extending task.
+        /// </summary>
         FAILED = 32
     }
 }
---
+++ TaskCat.SelfHost/TaskCat/Lib/JobTask/JobTaskExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskCat.Lib.JobTask
+{
+    using System;
+    using System.Linq.Expressions;
+    using Data.Entity;
+    using Data.Model;
+
+    public abstract class JobTaskExtension
+    {
+        /// <summary>
+        /// Type of the JobTask this extension is attached to
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Condition expression for the exntension to be activated
+        /// </summary>
+        public Expression<Func<JobTask, bool>> ConditionExpression { get; set; }
+
+        /// <summary>
+        /// Execute the extension works
+        /// </summary>
+        /// <param name="job"></param>
+        /// <returns></returns>
+        public abstract Job ExecuteExtension(Job job);
+    }
+}
