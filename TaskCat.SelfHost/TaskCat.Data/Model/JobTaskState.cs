a248afba45c47f02ee9a8cd5d003e5428b174c84
Swagata Prateek <thehoneymad@hotmail.com>
2017-01-27 15:01:56 +0100

    GFETCH-338: Fixed two more unit tests and now it's time to write unit tests for the new functionality

--- TaskCat.SelfHost/TaskCat.Data/Model/JobTaskState.cs
+++ TaskCat.SelfHost/TaskCat.Data/Model/JobTaskState.cs
@@ -1,35 +1,37 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace TaskCat.Data.Model
 {
     [Flags]
+    [DefaultValue(PENDING)]
     public enum JobTaskState
     {
         /// <summary>
         /// Task has not started yet
         /// </summary>
-        PENDING = 1,
+        PENDING = 0,
         /// <summary>
         /// Task is in progress
         /// </summary>
-        IN_PROGRESS = 2,
+        IN_PROGRESS = 1,
         /// <summary>
         /// Task is completed
         /// </summary>
-        COMPLETED = 4,
+        COMPLETED = 2,
         /// <summary>
         /// Task is cancelled, no default operation to follow.
         /// Incerementing attempt should be default.
         /// </summary>
-        CANCELLED = 8,
+        CANCELLED = 4,
         /// <summary>
         /// Task is returned, default operation is to return product to the sender.
         /// </summary>
-        RETURNED = 16,
+        RETURNED = 8,
         /// <summary>
         /// Task has failed, deafult operation is to increment the attempt.
         /// Can be customized for an extending task.
         /// </summary>
-        FAILED = 32
+        FAILED = 16
     }
 }
