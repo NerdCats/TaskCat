beb2f7d7c1b9a60dc3452ad9638a339ab8ea2720
riyad <ririyad.291@gmail.com>
2017-02-01 16:27:36 +0600

    GFETCH-329 Added necessary comment to describe the given Regex

--- TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
+++ TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
@@ -8,6 +8,7 @@
         public static string GetHrStateString(this JobTask task)
         {
             var stateString = task.State.ToString().Replace("_", " ").ToLowerInvariant();
+            //Regex to detect the capital letter as a starting of a word and insert an space between them 
             var presentTask = Regex.Replace(task.Type, "[A-Z]", " $0").Trim();
             return $"{presentTask} {stateString}";
         }
