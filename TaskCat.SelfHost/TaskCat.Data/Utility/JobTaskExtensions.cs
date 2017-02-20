c97bae63e25aff8a2421eac9c42d740c9c9f92f9
riyad <ririyad.291@gmail.com>
2017-02-20 13:15:44 +0600

    GFETCH-388 Added task variant in human readable job status

--- TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
+++ TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
@@ -7,10 +7,18 @@
     {
         public static string GetHrStateString(this JobTask task)
         {
+            var variantString = "";
+
+            if (!task.Variant.Equals("default"))
+            {
+                var taskVariantToString = task.Variant.ToString();
+                variantString = char.ToUpper(taskVariantToString[0]) + taskVariantToString.Substring(1);
+            }
+            
             var stateString = task.State.ToString().Replace("_", " ").ToLowerInvariant();
             //Regex to detect the capital letter as a starting of a word and insert an space between them 
             var presentTask = Regex.Replace(task.Type, "[A-Z]", " $0").Trim();
-            return $"{presentTask} {stateString}";
+            return $"{variantString} {presentTask} {stateString}";
         }
 
         public static bool IsConclusiveStateToMoveToNextTask(this JobTaskState state)
