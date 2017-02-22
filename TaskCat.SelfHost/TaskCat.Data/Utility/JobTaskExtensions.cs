012fe3e9fe7ecc9104bc6260eb9920a364fd006e
tareq89 <tareqaziz.aust.cse@gmail.com>
2017-02-22 11:21:27 +0600

    BUGFIX: trimmed whitespace from HRState

--- TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
+++ TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
@@ -19,7 +19,7 @@
             var stateString = task.State.ToString().Replace("_", " ").ToLowerInvariant();
             //Regex to detect the capital letter as a starting of a word and insert an space between them 
             var presentTask = Regex.Replace(task.Type, "[A-Z]", " $0").Trim();
-            return $"{variantString} {presentTask} {stateString}";
+            return $"{variantString} {presentTask} {stateString}".Trim();
         }
 
         public static bool IsConclusiveStateToMoveToNextTask(this JobTaskState state)
