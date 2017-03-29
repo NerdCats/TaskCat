b130f2ed7973fddc76625879b8d69f740fa38b0c
riyad <ririyad.291@gmail.com>
2017-03-29 12:22:32 +0600

    GFETCH-408 refactored code

--- TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
+++ TaskCat.SelfHost/TaskCat.Data/Utility/JobTaskExtensions.cs
@@ -9,16 +9,22 @@
         public static string GetHrStateString(this JobTask task)
         {
             var variantString = "";
+            var stateString = task.State.ToString().Replace("_", " ").ToLowerInvariant();
+            //Regex to detect the capital letter as a starting of a word and insert an space between them 
+            var presentTask = Regex.Replace(task.Type, "[A-Z]", " $0").Trim();
 
             if (task.Variant != null && !task.Variant.Equals("default"))
             {
                 TextInfo variantText = new CultureInfo("en-US", false).TextInfo;
                 variantString = variantText.ToTitleCase(task.Variant);
+
+                if (task.Variant.Equals("return"))
+                {
+                    presentTask = "";
+                    return $"Package {variantString.ToLower()} {stateString}";
+                }
             }
-            
-            var stateString = task.State.ToString().Replace("_", " ").ToLowerInvariant();
-            //Regex to detect the capital letter as a starting of a word and insert an space between them 
-            var presentTask = Regex.Replace(task.Type, "[A-Z]", " $0").Trim();
+
             return $"{variantString} {presentTask} {stateString}".Trim();
         }
 
