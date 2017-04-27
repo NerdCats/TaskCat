0ce0079b9c0519cf63665d6495ce984fd6795bc4
asadullahrifat89 <anonymusrif@gmail.com>
2017-04-27 13:15:23 +0600

    GFETCH-422 Controller return a single Json doc with results

--- TaskCat.SelfHost/TaskCat.Data/Model/Identity/Response/AssetJobCountModel.cs
+++ TaskCat.SelfHost/TaskCat.Data/Model/Identity/Response/AssetJobCountModel.cs
@@ -3,11 +3,30 @@
     using Entity.ShadowCat;
     using Entity.Identity;
 
-    public class AssetJobCountModel 
+    public class AssetJobCountModel
     {
         public AssetJobCountModel()
         {
-           
+
         }
+        public AssetJobCountModelBase Pick_Up { get; set; }
+        public AssetJobCountModelBase Delivery { get; set; }
+        public AssetJobCountModelBase Exp_Delivery { get; set; }
+    }
+
+    public class AssetJobCountModelBase
+    {
+        public AssetJobCountModelBase()
+        {
+            Assigned = 0;
+            Completed = 0;
+            Failed = 0;
+            Attempted = 0;
+        }
+
+        public long Assigned { get; set; } // total jobs assigned to the asset
+        public long Completed { get; set; }
+        public long Failed { get; set; }
+        public long Attempted { get; set; } // sum of completed and failed
     }
 }
--- TaskCat.SelfHost/TaskCat/Lib/AssetJobCount/AssetJobCountProvider.cs
+++ TaskCat.SelfHost/TaskCat/Lib/AssetJobCount/AssetJobCountProvider.cs
@@ -10,6 +10,10 @@
     using MongoDB.Bson;
     using TaskCat.Lib.AssetJobCount;
     using TaskCat.Data.Model.Identity.Response;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Data.Model;
+    using System.Collections.Generic;
 
     public class AssetJobCountProvider : IAssetJobCountProvider
     {
@@ -19,15 +23,46 @@
         {
             Collection = dbContext.Jobs;
         }
-        
+
         public async Task<AssetJobCountModel> FindEligibleAssetJobCounts(string AssetID)
         {
             if (string.IsNullOrWhiteSpace(AssetID)) throw new ArgumentNullException(nameof(AssetID));
 
             AssetJobCountModel ajcm = new AssetJobCountModel();
 
-            var result = await Collection.Find(x => x.Id == AssetID).FirstOrDefaultAsync();
-            if (result == null)
+            var jobsAssignedToAsset = await Collection.FindAsync(x => x.Tasks.Any(y => y.AssetRef == AssetID) && x.State == JobState.IN_PROGRESS);// all jobs assigned to this asset which are in progress
+
+            IEnumerable<Job> jList = jobsAssignedToAsset.ToEnumerable();// make them IEnumerable to help linq
+
+            #region Pick_Up
+
+            IEnumerable<Job> jList_Pick_Up = jList.Where(y => y.Tasks.Any(x => x.Type.Equals("PackagePickUp")));// filtering all PackagePickUp Type jobs
+
+            ajcm.Pick_Up.Assigned = jList_Pick_Up.Count();// total assigned PackagePickUp
+            ajcm.Pick_Up.Completed = jList_Pick_Up.Count(y => y.Tasks.Any(x => x.State == JobTaskState.COMPLETED));// total jobs COMPLETED
+            ajcm.Pick_Up.Failed = jList_Pick_Up.Count(y => y.Tasks.Any(x => x.State == JobTaskState.FAILED));// total jobs FAILED
+            ajcm.Pick_Up.Attempted = ajcm.Pick_Up.Completed + ajcm.Pick_Up.Failed; // sum of all jobs ATTEMPTED
+
+            #endregion
+
+            #region Delivery
+
+            IEnumerable<Job> jList_Delivery = jList.Where(y => y.Tasks.Any(x => x.Type.Equals("Delivery")));// filtering all Delivery Type jobs
+
+            ajcm.Delivery.Assigned = jList_Delivery.Count();// total assigned Delivery
+            ajcm.Delivery.Completed = jList_Delivery.Count(y => y.Tasks.Any(x => x.State == JobTaskState.COMPLETED));
+            ajcm.Delivery.Failed = jList_Delivery.Count(y => y.Tasks.Any(x => x.State == JobTaskState.FAILED));
+            ajcm.Delivery.Attempted = ajcm.Delivery.Completed + ajcm.Delivery.Failed;
+
+            #endregion
+
+            #region Exp_Delivery
+
+            // we do not have concrete properties which will signify Exp_Delivery jobs
+
+            #endregion
+
+            if (jobsAssignedToAsset == null)
             {
                 throw new EntityNotFoundException(typeof(AssetJobCountModel), AssetID);
             }
