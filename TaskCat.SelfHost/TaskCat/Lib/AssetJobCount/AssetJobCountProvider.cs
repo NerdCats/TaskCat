fa3a7fb148d5934e482a3032f6b5f27f394e53ce
asadullahrifat89 <anonymusrif@gmail.com>
2017-04-27 14:15:47 +0600

    GFETCH-422 changes in the AssetJobCount provider

--- TaskCat.SelfHost/TaskCat/Lib/AssetJobCount/AssetJobCountProvider.cs
+++ TaskCat.SelfHost/TaskCat/Lib/AssetJobCount/AssetJobCountProvider.cs
@@ -41,9 +41,9 @@
                 if (jList_Pick_Up != null)
                 {
                     ajcm.Pick_Up.Assigned = jList_Pick_Up.Count;// total assigned PackagePickUp
-                    ajcm.Pick_Up.Completed = jList_Pick_Up.Count(y => y.Tasks.Any(x => x.State == JobTaskState.COMPLETED));// total jobs COMPLETED
-                    ajcm.Pick_Up.Failed = jList_Pick_Up.Count(y => y.Tasks.Any(x => x.State == JobTaskState.FAILED));// total jobs FAILED
-                    ajcm.Pick_Up.Attempted = ajcm.Pick_Up.Completed + ajcm.Pick_Up.Failed; // sum of all jobs ATTEMPTED 
+                    ajcm.Pick_Up.Completed = jList_Pick_Up.Count(y => y.Tasks.Exists(x => x.State == JobTaskState.COMPLETED));// total jobs COMPLETED
+                    ajcm.Pick_Up.Failed = jList_Pick_Up.Count(y => y.Tasks.Exists(x => x.State == JobTaskState.FAILED));// total jobs FAILED
+                    ajcm.Pick_Up.Attempted = ajcm.Pick_Up.Completed + ajcm.Pick_Up.Failed > ajcm.Pick_Up.Assigned ? ((ajcm.Pick_Up.Completed > ajcm.Pick_Up.Failed) ? ajcm.Pick_Up.Completed : ajcm.Pick_Up.Failed) : ajcm.Pick_Up.Completed + ajcm.Pick_Up.Failed; // sum of all jobs ATTEMPTED 
                 }
 
                 #endregion
@@ -55,9 +55,9 @@
                 if (jList_Delivery != null)
                 {
                     ajcm.Delivery.Assigned = jList_Delivery.Count;// total assigned Delivery
-                    ajcm.Delivery.Completed = jList_Delivery.Count(y => y.Tasks.Any(x => x.State == JobTaskState.COMPLETED));
-                    ajcm.Delivery.Failed = jList_Delivery.Count(y => y.Tasks.Any(x => x.State == JobTaskState.FAILED));
-                    ajcm.Delivery.Attempted = ajcm.Delivery.Completed + ajcm.Delivery.Failed;
+                    ajcm.Delivery.Completed = jList_Delivery.Count(y => y.Tasks.Exists(x => x.State == JobTaskState.COMPLETED));
+                    ajcm.Delivery.Failed = jList_Delivery.Count(y => y.Tasks.Exists(x => x.State == JobTaskState.FAILED));
+                    ajcm.Delivery.Attempted = ajcm.Delivery.Completed + ajcm.Delivery.Failed > ajcm.Delivery.Assigned ? ((ajcm.Delivery.Completed > ajcm.Delivery.Failed) ? ajcm.Delivery.Completed : ajcm.Delivery.Failed) : ajcm.Delivery.Completed + ajcm.Delivery.Failed; // sum of all jobs ATTEMPTED 
                 }
 
                 #endregion
