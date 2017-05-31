c72a78be8663d98033ea2a55621b2e619e06708d
Swagata Prateek <thehoneymad@hotmail.com>
2017-05-31 21:57:57 +0200

    Putting a required tag over the Value so it doesn't miss at all

--- TaskCat.SelfHost/TaskCat.Data/Entity/DataTag.cs
+++ TaskCat.SelfHost/TaskCat.Data/Entity/DataTag.cs
@@ -1,9 +1,11 @@
 ﻿namespace TaskCat.Data.Entity
 {
     using MongoDB.Bson.Serialization.Attributes;
+    using System.ComponentModel.DataAnnotations;
 
     public class DataTag : DbEntity
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Data tag value is not provided")]
         [BsonRequired]
         public string Value { get; set; }
     }
