b444c48f01ec4210da93e5c37887b0a0b3540bee
Swagata Prateek~ <thehoneymad@hotmail.com~>
2018-03-14 05:43:09 -0700

    First commit on JobBackgroundService
    Started adding the basic works necessary for background
    services that will timely poll jobs from our partners and
    create orders in our system

---
+++ TaskCat.SelfHost/TaskCat.BackgroundJobService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting.;
+
+namespace TaskCat.BackgroundJobService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new Microsoft.Extensions.Hosting. HostBuilder();
+        }
+    }
+}
