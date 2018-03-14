3b7a62207c295e60308bfb48e13886a78a3b25e3
Swagata Prateek~ <thehoneymad@hotmail.com~>
2018-03-14 06:57:38 -0700

    Added update-order operation for infini services

--- TaskCat.SelfHost/TaskCat.PartnerServices/Infini/OrderService.cs
+++ TaskCat.SelfHost/TaskCat.PartnerServices/Infini/OrderService.cs
@@ -18,11 +18,53 @@ namespace TaskCat.PartnerServices.Infini
             _httpClient = httpClient ?? throw new System.ArgumentNullException(nameof(httpClient));
         }
 
+        public async Task<OrderStatus> UpdateOrderStatus(string token, string order_id, string order_status)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentException("Invalid token provided", nameof(token));
+            }
+
+            if (string.IsNullOrWhiteSpace(order_id))
+            {
+                throw new ArgumentException("Invalid order_id provided", nameof(order_id));
+            }
+
+            if (string.IsNullOrWhiteSpace(order_status))
+            {
+                throw new ArgumentException("Invalid order_status provided", nameof(order_status));
+            }
+
+            var uriBuilder = new UriBuilder(baseUri);
+            uriBuilder.Path = "api/orders/api-update-order-status";
+
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["order_status"] = order_status;
+            query["order_id"] = order_id;
+            uriBuilder.Query = query.ToString();
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = uriBuilder.Uri,
+                Method = HttpMethod.Post
+            };
+
+            request.Headers.Add("token", token);
+
+            var response = await this._httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var returnVal = JsonConvert.DeserializeObject<OrderStatus>(responseJson);
+
+            return returnVal;
+        }
+
         public async Task<IEnumerable<Order>> GetOrders(string token, string orderStatus = null)
         {
-            if (token == null)
+            if (string.IsNullOrWhiteSpace(token))
             {
-                throw new ArgumentNullException(nameof(token));
+                throw new ArgumentException("Invalid token provided", nameof(token));
             }
 
             var uriBuilder = new UriBuilder(baseUri);
@@ -38,7 +80,7 @@ namespace TaskCat.PartnerServices.Infini
             var request = new HttpRequestMessage()
             {
                 RequestUri = uriBuilder.Uri,
-                Method = HttpMethod.Get
+                Method = HttpMethod.Post
             };
 
             request.Headers.Add("token", token);
